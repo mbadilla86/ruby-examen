aeacd8a7986c36fde3e5c569c68ad4230a8be972
Mauricio Badilla <mbadilla86@gmail.com>
2021-02-15 06:02:12 -0600

    ingreso img

--- examen.rb
+++ examen.rb
@@ -3,8 +3,7 @@ require "net/http"
 require "json"
 
 def request(url,token = nil)
-    sol = ARGV[0]
-    url = URI("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=#{sol}1000&api_key=d4v6gZycznoxIBp3HGJJlyRPYFvht48pSVTyXMjM")
+    url = URI("#{url}api_key=#{token}")
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(url)
@@ -12,10 +11,22 @@ def request(url,token = nil)
     return JSON.parse(response.read_body)
 end
 
-#index.html
+
 def buid_wed_page(info_hash)
     File.open('index.html', 'w') do |file|      
-        file.puts "<img src='#{info_hash["photos"]}"
+        file.puts "<html>"
+        file.puts "<head>"
+        file.puts "</head>"
+        file.puts "<body>"
+        file.puts "<ul>"
+        info_hash["photos"].each do |photo_hash|
+             file.puts "<li><img src='#{photo_hash["img_src"]} 'width = '110'></li>"
+
+        end
+        file.puts "<li> 'Largo': #{info_hash["photos"].length}</li>" 
+        file.puts "</ul>"
+        file.puts "</body>"
+        file.puts "</html>"
     end
 end
 nasa_array = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&","d4v6gZycznoxIBp3HGJJlyRPYFvht48pSVTyXMjM")
